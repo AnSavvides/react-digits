eff7e858eb89c0de5f2131c79d5dbbfcf1d47d7b
Andreas Savvides <asavvides@twitter.com>
2015-08-07 23:32:36 +0100

    Just return content

--- server.py
+++ server.py
@@ -4,7 +4,6 @@ import json
 from flask import Flask
 from flask import request
 from flask import abort
-from flask import jsonify
 
 app = Flask("ReactDigitsServer", static_url_path="", static_folder="./")
 app.add_url_rule("/", "root", lambda: app.send_static_file("index.html"))
@@ -22,6 +21,6 @@ def verify_user():
     if response.status_code != 200:
         return abort(response.status_code)
 
-    return jsonify(json.loads(response.content))
+    return response.content
 
 app.run(debug=True)
